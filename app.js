c0abd371d997d879b8f78aa6f2e92608d4f62e15
gwonyejun <kyjuni1059@gmail.com>
2023-03-16 17:49:15 +0900

    문자열에 텍스트 값을 유니코드 값으로 바꾸어 주었다

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 const mysql = require('mysql');  // mysql 모듈 로드
 const conn = {  // mysql 접속 설정
   host: 'localhost',
-  port: '3306',
   user: 'root',
   password: '1234',
   database: 'test'
 };
 
+
+
+
+
+
+
 var connection = mysql.createConnection(conn); // DB 커넥션 생성
 connection.connect();   // DB 접속
 
-var testQuery = "INSERT INTO `testtable` (`num`,`name`,`usr_id`,`usr_pw`) VALUES ('1','js','연결','성공?');";
+let test;
+
+var testQuery = "INSERT INTO `menber` (`num`,`name`,`id`,`pw`,`jumin`) VALUES ('12','권예준','sag7000','1234','1234512354');";
 
 connection.query(testQuery, function (err, results, fields) { // testQuery 실행
   if (err) {
     console.log(err);
   }
   console.log(results);
+
 });
 
-testQuery = "SELECT * FROM testtable";
+testQuery = "SELECT * FROM menber";
 
 connection.query(testQuery, function (err, results, fields) { // testQuery 실행
   if (err) {
     console.log(err);
   }
   console.log(results);
+ 
+test = results;
 });
+
 
 
 connection.end(); // DB 접속 종료
